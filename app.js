34dba61bd912b61e0b1dd3c5d38730854f22f9f2
Nitai <nitai@helpmonks.com>
2026-05-20 21:03:02 -0400

    Further Clickstack integration

--- app.js
+++ app.js
@@ -69,6 +69,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.locals.openpanel = config.openpanel;
 app.locals.openobserve = config.openobserve;
 app.locals.sentry = config.sentry;
+app.locals.hyperdx = config.hyperdx;
 installIconLocals(app);
 
 // Cache-busting build ID — embedded in static asset paths: /static/v-{hash}/...
